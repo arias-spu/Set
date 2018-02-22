ede362abb7a016015c6fd4708d12a58781e46051
Carlos R Arias <arias@spu.edu>
2018-02-21 16:37:10 -0800

    Incomplete Set.cpp

---
+++ set.cpp
@@ -0,0 +1,24 @@
+#include "set.h"
+
+#include <string>
+
+using std::string;
+
+
+Set::Set(int maxElement){
+}
+Set::Set(const Set& other){
+}
+Set::~Set(){
+}
+Set& Set::operator=(const Set& rhs){
+}
+string Set::ToString()const{
+}
+bool Set::AddElement(unsigned int){
+}
+bool Set::RemoveElement(unsigned int){
+}
+bool Set::Contains(unsigned int){
+}
+	
